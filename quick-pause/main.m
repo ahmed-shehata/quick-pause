95461067d5a5384af55d9e51820fd9c328681e6e
Ahmed Shehata <4982099+ahmed-shehata@users.noreply.github.com>
2019-12-14 17:23:07 +0000

    Initial Commit

---
+++ quick-pause/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  quick-pause
+//
+//  Created by Ahmed Shehata on 12/14/19.
+//  Copyright © 2019 Ahmed Shehata. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+  @autoreleasepool {
+      // insert code here...
+      NSLog(@"Hello, World!");
+  }
+  return 0;
+}
