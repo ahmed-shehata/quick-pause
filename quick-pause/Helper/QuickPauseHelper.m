3c12a11c672ae755171aa44399cf3ad7c8450886
Ahmed Shehata <4982099+ahmed-shehata@users.noreply.github.com>
2020-01-02 11:28:17 +0000

    fix formatting

--- quick-pause/Helper/QuickPauseHelper.m
+++ quick-pause/Helper/QuickPauseHelper.m
@@ -16,43 +16,43 @@ NSString *const kMacSpeakerUID = @"BuiltInSpeakerDevice";
 @implementation QuickPauseHelper : NSObject
 
 - (id)init {
-    self = [super init];
-    if (self) {
-      [self setPlayer:[[BTHSpotifyInterface alloc] init]];
-    }
-    return self;
+  self = [super init];
+  if (self) {
+    [self setPlayer:[[BTHSpotifyInterface alloc] init]];
+  }
+  return self;
 }
 
 - (NSString *)getCurrentOutputDeviceUID
 {
-    NSString *deviceUID = @"";
+  NSString *deviceUID = @"";
 
-    AudioObjectPropertyAddress aopa;
-    aopa.mSelector = kAudioHardwarePropertyDevices;
-    aopa.mScope = kAudioObjectPropertyScopeGlobal;
-    aopa.mElement = kAudioObjectPropertyElementMaster;
+  AudioObjectPropertyAddress aopa;
+  aopa.mSelector = kAudioHardwarePropertyDevices;
+  aopa.mScope = kAudioDevicePropertyScopeOutput;
+  aopa.mElement = kAudioObjectPropertyElementMaster;
 
-    UInt32 propSize;
-    OSStatus error = AudioObjectGetPropertyDataSize(kAudioObjectSystemObject, &aopa, 0, NULL, &propSize);
+  UInt32 propSize;
+  OSStatus error = AudioObjectGetPropertyDataSize(kAudioObjectSystemObject, &aopa, 0, NULL, &propSize);
+  if (error == noErr) {
+    int deviceCount = propSize / sizeof(AudioDeviceID);
+    AudioDeviceID *audioDevices = (AudioDeviceID *)malloc(propSize);
+    error = AudioObjectGetPropertyData(kAudioObjectSystemObject, &aopa, 0, NULL, &propSize, audioDevices);
     if (error == noErr) {
-        int deviceCount = propSize / sizeof(AudioDeviceID);
-        AudioDeviceID *audioDevices = (AudioDeviceID *)malloc(propSize);
-        error = AudioObjectGetPropertyData(kAudioObjectSystemObject, &aopa, 0, NULL, &propSize, audioDevices);
+      UInt32 propSize = sizeof(CFStringRef);
+      for(int i = 1; i <= deviceCount; i++) {
+        NSString *result;
+        aopa.mSelector = kAudioDevicePropertyDeviceUID;
+        error = AudioObjectGetPropertyData(audioDevices[i], &aopa, 0, NULL, &propSize, &result);
         if (error == noErr) {
-            UInt32 propSize = sizeof(CFStringRef);
-            for(int i = 1; i <= deviceCount; i++) {
-                NSString *result;
-                aopa.mSelector = kAudioDevicePropertyDeviceUID;
-                error = AudioObjectGetPropertyData(audioDevices[i], &aopa, 0, NULL, &propSize, &result);
-                if (error == noErr) {
-                  deviceUID = result;
-                  break;
-                }
-            }
+          deviceUID = result;
+          break;
         }
-        free(audioDevices);
+      }
     }
-    return deviceUID;
+    free(audioDevices);
+  }
+  return deviceUID;
 }
 - (void) checkPlaybackDevices {
   NSString* uid = [self getCurrentOutputDeviceUID];
