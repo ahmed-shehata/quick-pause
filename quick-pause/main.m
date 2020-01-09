93ae1c6663ed93f6a50424a626802457ec19194e
Ahmed Shehata <4982099+ahmed-shehata@users.noreply.github.com>
2020-01-09 10:53:35 +0000

    Lowered interval so CPU usage is lower

--- quick-pause/main.m
+++ quick-pause/main.m
@@ -18,7 +18,7 @@ int main(int argc, const char * argv[]) {
 
     NSDate *now = [[NSDate alloc] init];
     NSTimer *timer = [[NSTimer alloc] initWithFireDate:now
-    interval:.01
+    interval:.5
     target:helper
     selector:@selector(pauseIfDefaultDeviceChanged)
     userInfo:nil
